01d971a9992256d51f422784fe9ef5d531f6403c
wcardoso <weverton.cardoso@hotmail.com>
2021-06-20 00:06:48 -0300

    layouts e styles

--- app/src/main/java/br/com/wevs/cardoso/presentation/MainActivity.kt
+++ app/src/main/java/br/com/wevs/cardoso/presentation/MainActivity.kt
@@ -2,9 +2,14 @@ package br.com.wevs.cardoso.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import br.com.wevs.cardoso.R
 
 class MainActivity : AppCompatActivity() {
+
+    private val controlador by lazy {
+        findNavController(R.id.main_activity_nav_host)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/br/com/wevs/cardoso/presentation/fragment/FragmentListJava.kt
@@ -0,0 +1,23 @@
+package br.com.wevs.cardoso.presentation.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import br.com.wevs.cardoso.R
+
+class FragmentListJava : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.fragment_list_java,
+            container,
+            false
+        )
+    }
+}
